a475052bdc280acbeaeaf8f765f47ed6a2305648
Jane Eisenstein <ejane@softweave.com>
2021-01-12 14:06:17 -0500

    Removes obsolete simpl option.

--- {{cookiecutter.repo_slug}}/js/modules/Root.js
+++ {{cookiecutter.repo_slug}}/js/modules/Root.js
@@ -51,6 +51,5 @@ export default simpl({
   progressComponent: Progress,
   root_topic: ROOT_TOPIC,
   topics: () => topics,
-  loadAllScenarios: false,
-  loadWorldResults: true
+  loadAllScenarios: false
 })(RootContainer);
